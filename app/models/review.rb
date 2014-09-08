68d8c16f765f3fa43988f7dac0cd89b5684e0513
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-09-08 22:45:37 +0200

    Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :user_id, presence: true
+
   belongs_to :product
   belongs_to :user
 end
