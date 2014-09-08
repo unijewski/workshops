d5aa37d0857e70b51e52690ba25e1ffd9acce319
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-09-08 22:37:48 +0200

    Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,23 @@
 class Product < ActiveRecord::Base
+
+  VALID_PRICE_REGEX = /\A\d+(?:\.\d{0,2})?\z/
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true,
+                    numericality: true,
+                    format: { with: VALID_PRICE_REGEX }
+
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  def average_rating
+    average = 0.0
+    self.reviews.each do |rev|
+      average += rev.rating
+    end
+
+    average / self.reviews.count
+  end
 end
