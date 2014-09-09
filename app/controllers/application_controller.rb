19a9616313b9e5bbd9fa24105618fc5a6e376037
Grzegorz Unijewski <grzegorz@unijewski.pl>
2014-09-09 23:24:22 +0200

    Fixed registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :devise_names_configuration
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
   protect_from_forgery with: :exception
+
+  protected
+
+  def devise_names_configuration
+    if devise_controller?
+      devise_parameter_sanitizer.for(:sign_up) << :firstname << :lastname
+    end
+  end
 end
